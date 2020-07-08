94c521f99c331951f102f031c602a519eaeb4241
Qingyu Deng <dengqingyu@yidian-inc.com>
2020-07-08 16:15:18 +0800

    feat: update case

--- cypress/integration/failure.spec.js
+++ cypress/integration/failure.spec.js
@@ -8,6 +8,14 @@ describe("wrong wrap", () => {
   });
 
   it("should be able to use screenshot", () => {
+    cy.visit("http://127.0.0.1:5500/index.html");
     cy.get("body").takeScreenshot();
+  });
+
+  it("should work too", () => {
+    cy.visit("http://127.0.0.1:5500/index.html");
+    cy.get("body").then(($el) => {
+      cy.wrap($el).screenshot();
+    });
   });
 });
